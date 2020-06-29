c5cf0c46735901ca378450a7c8a9d0039496c128
Timothy Flavin <BinaryOwlizard@gmail.com>
2020-06-29 11:22:53 -0500

    Realized that accuracy was not a good judge of my output because I was giving it all 1s every time and accuracy judges of the net made the right choice but I needed mean squared error

--- DenseSnakeAI.py
+++ DenseSnakeAI.py
@@ -4,29 +4,27 @@ import numpy as np
 class DenseSnakeModel(tf.keras.Model):
   def __init__(self, width, height):
     super(DenseSnakeModel, self).__init__()
-    self.dense1 = tf.keras.layers.Dense(width*height*4, activation=tf.nn.leaky_relu, input_shape=(width*height*4,))
-    self.dense2 = tf.keras.layers.Dense(width*height*2, activation=tf.nn.sigmoid)
-    self.dense3 = tf.keras.layers.Dense(width*height, activation=tf.nn.sigmoid)
-    self.dense4 = tf.keras.layers.Dense(4, activation=tf.nn.softmax)
+    self.dense1 = tf.keras.layers.Dense(width*height*2, activation=tf.nn.sigmoid)
+    self.dense2 = tf.keras.layers.Dense(width*height, activation=tf.nn.sigmoid)
+    self.dense3 = tf.keras.layers.Dense(4, activation=tf.nn.sigmoid)
     self.dropout = tf.keras.layers.Dropout(0.5)
 
   def call(self, inputs, training=False):
     x = self.dense1(inputs)
     x = self.dense2(x)
-    x = self.dense3(x)
     if training:
       x = self.dropout(x, training=training)
-    return self.dense4(x)
+    return self.dense3(x)
 
 model = DenseSnakeModel(10,10)
-model.compile(loss=tf.keras.losses.mean_squared_error,
+model.compile(loss=tf.keras.losses.MeanSquaredError(),
               optimizer=tf.keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+              metrics=['mean_squared_error'])
 
-model.fit(x=np.ones((1,400), int), 
-          y=np.ones((1,4), int), 
-          epochs=1,
-          batch_size=1,
+model.fit(x=np.ones((100000,400), int), 
+          y=np.ones((100000,4), int), 
+          epochs=3,
+          batch_size=64,
           verbose=1,
           validation_split=0.0,
           validation_data=None,
